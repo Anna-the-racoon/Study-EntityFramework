eb832f405c8fd8f699aeecd8d620b89fa16aa4ff
wwww.anechka@gmail.com <wwww.anechka@gmail.com>
2021-10-22 23:17:22 +0300

    Add project files.

---
+++ Entity_Framework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Entity_Framework
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            using(var db = new UserContext())
+            {
+                //create 2 object User
+                var user1 = new User { Name = "Anna", Age = 23 };
+                var user2 = new User { Name = "Alex", Age = 30 };
+
+                // put them to the database
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Save");
+
+                //getting objects from the database and output them to the console
+                var users = db.Users;
+                Console.WriteLine("List of employees:");
+                foreach(var u in users)
+                {
+                    Console.WriteLine($"{u.Id}. {u.Name}, age - {u.Age}");
+                }
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ Entity_Framework/UserContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace Entity_Framework
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base("DbConnection")  // определяет контекст данных, используемый для взаимодействия с базой данных.
+        { }
+        public DbSet<User> Users { get; set; }
+    }
+}
